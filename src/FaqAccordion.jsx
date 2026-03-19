8c09da26a35d5c9b7eb2947445c955812ad3f2bc
Saliva-sys <aweidlichova@gmail.com>
2026-03-19 20:59:47 +0100

    style: layout and custom variable fonts setup

--- src/FaqAccordion.jsx
+++ src/FaqAccordion.jsx
@@ -2,17 +2,30 @@ import './FaqAccordion.css';
 import { useState, useEffect } from 'react';
 
 // 2. KÓD KOMPONENTU (telo funkcie)
+const faqData = [
+    {
+        id: 1, 
+        title: "What is Frontend Mentor, and how will it help me?", 
+        answer: "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building."},
+    {
+        id: 2, 
+        title: "Is Frontend Mentor free?", 
+        answer: "Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels."},
+    {
+        id: 3, 
+        title: "Can I use Frontend Mentor projects in my portfolio?", 
+        answer: "Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!"},
+    {
+        id: 4, 
+        title: "How can I get help if I'm stuck on a challenge?", 
+        answer: "The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members."}
+  ]
+
 const FaqAccordion = () => {
   useEffect(() => {
     document.title = "Saliva-sys | FAQ accordion";
   }, []); // [] znamená, že sa to zmení len pri otvorení tohto komponentu
-
-  const faqData = [
-    {id: 1, title: "What is Frontend Mentor, and how will it help me?", answer: "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It&#39;s suitable for all levels and ideal for portfolio building."},
-    {id: 2, title: "Is Frontend Mentor free?", answer: "Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels."},
-    {id: 3, title: "Can I use Frontend Mentor projects in my portfolio?", answer: "Yes, you can use projects completed on Frontend Mentor in your portfolio. It&#39;s an excellent way to showcase your skills to potential employers!"},
-    {id: 4, title: "How can I get help if I&#39;m stuck on a Frontend Mentor challenge?", answer: "The best place to get help is inside Frontend Mentor&#39;s Discord community. There&#39;s a help channel where you can ask questions and seek support from other community members."}
-  ]
+  
 
   const [openId, setOpenId] =useState(null);
 
@@ -24,8 +37,8 @@ const FaqAccordion = () => {
     <main>
         <div className="main__space">
             <h1 className="faq__main-title">
-                <img src="./icon-star.svg" alt="star"/>
-                FAQs</h1>
+                <img src="./icon-star.svg" className="title__icon" alt="star"/>
+                <span className="title__faq"> FAQs</span></h1>
 
             <section className="faq__space">
                 {faqData.map((item) => (
@@ -35,8 +48,8 @@ const FaqAccordion = () => {
                             <h2 className="faq__title">{item.title}</h2>
                             <span className="faq__icon">
                                 {openId === item.id 
-                                    ? <img src="./icon-plus.svg" alt="plus" /> 
-                                    : <img src="./icon-minus.svg" alt="minus"/>
+                                    ? <img src="./icon-minus.svg" alt={openId === item.id ? "collapse" : "expand"} /> 
+                                    : <img src="./icon-plus.svg" alt={openId === item.id ? "collapse" : "expand"}/>
                                 }
                             </span>
                         </button>
@@ -44,9 +57,10 @@ const FaqAccordion = () => {
                         {openId === item.id && (
                             <p className="faq__answer">{item.answer}</p>
                         )}
-                    </article>
-                ))}
-            </section>
+                        <hr className="faq__divider" />
+                    </article>                    
+                ))}                
+            </section>            
         </div>
 
         <footer className="attribution">
